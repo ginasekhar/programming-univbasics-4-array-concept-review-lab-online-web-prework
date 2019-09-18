02b29111afec91f7441cf556665901737cd1dc30
Gina Sekhar <ginasekhar@yahoo.com>
2019-09-18 14:11:00 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,7 +10,12 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  
+  len.times do |index|
+    if array[index] == value_to_find
+      return index
+    end
+  end
+  return nil
 end
 
 def find_min_value(array)
